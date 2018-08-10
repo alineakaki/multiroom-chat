4c829dee36b10f4acc521e0c73b38d628bdeeeef
Aline Akaki <alineakaki1@gmail.com>
2018-08-10 15:12:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+// importar as config do servidor 
+
+var app = require('./config/server');
+
+// parametrizar a porta de escuta do servidor 
+var server = app.listen(80, function() {
+    console.log("Server no ar");
+});
+
+var io = require('socket.io').listen(server);
+app.set('io', io);
+
+// criar a conexão por websocket 
+io.on('connection', function(socket){
+        console.log('Usuario conectou');
+
+        socket.on('disconnect', function() {
+        console.log('Usuário desconectou');
+    })
+
+        // dialogo
+        socket.on('msgParaServidor', function(data) {
+            socket.emit(
+                'msgParaCliente', 
+                {apelido: data.apelido, mensagem: data.mensagem}
+            );
+
+            socket.broadcast.emit(
+                'msgParaCliente', 
+                {apelido: data.apelido, mensagem: data.mensagem}
+            );
+        
+        // participantes 
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit(
+                'participantesParaCliente', 
+                {apelido: data.apelido}
+            );
+
+            socket.broadcast.emit(
+                'participantesParaCliente', 
+                {apelido: data.apelido}
+            );
+        }
+     });
+});
